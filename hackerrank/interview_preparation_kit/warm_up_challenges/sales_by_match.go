632c7544b11630452980b38b9e5ad3356b355822
chris-mlvz <chris.mlvz@gmail.com>
2021-08-29 18:07:21 -0500

    Sales by Match - Passed

---
+++ hackerrank/interview_preparation_kit/warm_up_challenges/sales_by_match.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n int32, ar []int32) int32 {
+	// Write your code here
+	var total int32
+	dict := make(map[int32]int32)
+	for i := 0; int32(i) < n; i++ {
+		if _, ok := dict[ar[i]]; ok {
+			dict[ar[i]]++
+		} else {
+			dict[ar[i]] = 1
+		}
+	}
+	for key, v := range dict {
+		fmt.Println(key, v)
+		if v >= 2 {
+			total += v / 2
+		}
+	}
+	return total
+}
+func main() {
+	total := sockMerchant(int32(20), []int32{4, 5, 5, 5, 6, 6, 4, 1, 4, 4, 3, 6, 6, 3, 6, 1, 4, 5, 5, 5})
+	fmt.Println(total)
+}
+
+// func main() {
+// 	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+// 	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+// 	checkError(err)
+
+// 	defer stdout.Close()
+
+// 	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+// 	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+// 	checkError(err)
+// 	n := int32(nTemp)
+
+// 	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+// 	var ar []int32
+
+// 	for i := 0; i < int(n); i++ {
+// 		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+// 		checkError(err)
+// 		arItem := int32(arItemTemp)
+// 		ar = append(ar, arItem)
+// 	}
+
+// 	result := sockMerchant(n, ar)
+
+// 	fmt.Fprintf(writer, "%d\n", result)
+
+// 	writer.Flush()
+// }
+
+// func readLine(reader *bufio.Reader) string {
+// 	str, _, err := reader.ReadLine()
+// 	if err == io.EOF {
+// 		return ""
+// 	}
+
+// 	return strings.TrimRight(string(str), "\r\n")
+// }
+
+// func checkError(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
