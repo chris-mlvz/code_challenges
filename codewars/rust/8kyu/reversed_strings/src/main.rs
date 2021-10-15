8ab5e9ce5a8800f04cb69b3e76fc00f374064217
chris-mlvz <chris.mlvz@gmail.com>
2021-10-15 18:30:25 -0500

    Rust - 8kyu - Reversed Strings

---
+++ codewars/rust/8kyu/reversed_strings/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {}
+
+fn solution(phrase: &str) -> String {
+    phrase.chars().rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn sample_test() {
+        assert_eq!(solution("world"), "dlrow");
+    }
+}
