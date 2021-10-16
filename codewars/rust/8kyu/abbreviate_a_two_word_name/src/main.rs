63dc5fa28768598b05ebb602b88a96a51e3fe2ee
chris-mlvz <chris.mlvz@gmail.com>
2021-10-15 20:57:14 -0500

    Rust - 8kyu - Abbreviate a Two Word Name

---
+++ codewars/rust/8kyu/abbreviate_a_two_word_name/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {}
+
+fn abbrev_name(name: &str) -> String {
+    name.split_whitespace()
+        .map(|x| x.chars().nth(0).unwrap().to_string().to_uppercase())
+        .collect::<Vec<String>>()
+        .join(".")
+}
+// Rust test example:
+#[test]
+fn sample_tests() {
+    assert_eq!(abbrev_name("Sam Harris"), "S.H");
+    assert_eq!(abbrev_name("Patrick Feenan"), "P.F");
+    assert_eq!(abbrev_name("Evan Cole"), "E.C");
+    assert_eq!(abbrev_name("P Favuzzi"), "P.F");
+    assert_eq!(abbrev_name("David Mendieta"), "D.M");
+}
