733875170bb5db47d37176cee9c8f56b14cb608b
chris-mlvz <chris.mlvz@gmail.com>
2021-10-15 20:29:43 -0500

    Rust - Kyu8 - number to reversed array of digits

---
+++ codewars/rust/8kyu/convert_number_to_reversed_array_of_digits/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+fn digitize(n: u64) -> Vec<u8> {
+    n.to_string()
+        .chars()
+        .rev()
+        .map(|x| x.to_digit(10).unwrap() as u8)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fixed() {
+        assert_eq!(digitize(35231), vec![1, 3, 2, 5, 3]);
+    }
+}
