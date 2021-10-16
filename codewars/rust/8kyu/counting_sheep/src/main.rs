89ecbb83ec0a093601dcf01980a63ce1ad87029d
chris-mlvz <chris.mlvz@gmail.com>
2021-10-15 19:54:02 -0500

    Rust - 8kyu - Counting sheep...

---
+++ codewars/rust/8kyu/counting_sheep/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {}
+
+fn count_sheep(sheep: &[bool]) -> u8 {
+    sheep.iter().filter(|&x| *x).count() as u8
+}
+
+#[test]
+fn returns_correct_sheep_count() {
+    assert_eq!(count_sheep(&[false]), 0);
+    assert_eq!(count_sheep(&[true]), 1);
+    assert_eq!(count_sheep(&[true, false]), 1);
+}
