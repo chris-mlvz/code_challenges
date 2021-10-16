9e6dd4b86f69d2400f84ee81f6cd81a6345e340c
chris-mlvz <chris.mlvz@gmail.com>
2021-10-15 19:01:54 -0500

    Rust - 8kyu - Convert a Number to a String

---
+++ codewars/rust/8kyu/number_to_string/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {}
+
+fn number_to_string(i: i32) -> String {
+    i.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::number_to_string;
+
+    #[test]
+    fn returns_number_as_a_string() {
+        assert_eq!(number_to_string(67), "67");
+        assert_eq!(number_to_string(1 + 2), "3");
+    }
+}
